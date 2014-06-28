af8f7502fd908c148559a8f3a10ef49e89fcabba
ZoliEngineer <juzoli@gmail.com>
2014-06-28 18:05:55 +0200

    streams working

---
+++ streams/src/main/scala/streams/SandBox.sc
@@ -0,0 +1,25 @@
+package streams
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import Bloxorz._
+
+object SandBox extends GameDef with Solver with StringParserTerrain{
+  
+    val level =
+    """ooo-------
+      |oSoooo----
+      |ooooooooo-
+      |-ooooooooo
+      |-----ooToo
+      |------ooo-""".stripMargin
+
+   
+    
+    pathsFromStart
+  
+  
+
+ 
+}
--- streams/src/test/scala/streams/BloxorzSuite.scala
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -52,16 +52,40 @@ class BloxorzSuite extends FunSuite {
       assert(startPos == Pos(1,1))
     }
   }
+  
+  test("neighbours with history"){
+    new Level1 {
+      assert(neighborsWithHistory(Block(Pos(1,1),Pos(1,1)), List(Left,Up)).toSet
+          === Set(
+		  (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+		  (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+		))
+    }
+  }
+  
+    test("new neighbors only"){
+    new Level1 {
+      assert(
+          newNeighborsOnly(
+			  Set(
+			    (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+			    (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+			  ).toStream,			
+			  Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))).toSet
+		 ===  Set((Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))))
+    }
+  }
+  
 
   test("optimal solution for level 1") {
     new Level1 {
-      assert(solve(solution) == Block(goal, goal))
+      assert(solve(solution) === Block(goal, goal))
     }
   }
 
   test("optimal solution length for level 1") {
     new Level1 {
-      assert(solution.length == optsolution.length)
+      assert(solution.length === optsolution.length)
     }
   }
 }
